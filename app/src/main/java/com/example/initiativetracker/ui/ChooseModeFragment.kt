59d788f9a6030ef55e9f23a34fb6302352b207d1
JONAS VERMASSEN <jonas.vermassen.y1164@student.hogent.be>
2019-12-26 14:57:40 +0100

    navigation to second screen

--- app/src/main/java/com/example/initiativetracker/ui/ChooseModeFragment.kt
+++ app/src/main/java/com/example/initiativetracker/ui/ChooseModeFragment.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.initiativetracker.R
 import com.example.initiativetracker.databinding.FragmentChooseModeBinding
+import kotlinx.android.synthetic.main.fragment_choose_mode.btn_choose_join
 
 
 class ChooseModeFragment : Fragment() {
+
+    lateinit var btn_join: Button
 
 
     override fun onCreateView(
@@ -25,4 +30,23 @@ class ChooseModeFragment : Fragment() {
         )
         return binding.root
     }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_join = btn_choose_join
+
+
+        btn_join.setOnClickListener { v: View ->
+
+
+            v.findNavController()
+                .navigate(R.id.action_choose_mode_to_session_code_enter)
+
+        }
+    }
+
+
+
 }
--- app/src/main/java/com/example/initiativetracker/ui/SessionCodeFragment.kt
+++ app/src/main/java/com/example/initiativetracker/ui/SessionCodeFragment.kt
@@ -1,6 +1,25 @@
 package com.example.initiativetracker.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.example.initiativetracker.R
+import com.example.initiativetracker.databinding.FragmentSessionCodeEnterBinding
 
 class SessionCodeFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding = DataBindingUtil.inflate<FragmentSessionCodeEnterBinding>(
+            inflater,
+            R.layout.fragment_session_code_enter,
+            container,
+            false
+        )
+        return binding.root
+    }
 }
