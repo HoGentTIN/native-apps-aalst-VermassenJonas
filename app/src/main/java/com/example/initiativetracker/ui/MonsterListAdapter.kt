6572f2b0cad8df0a7c904c4c8435b91555a1f709
JONAS VERMASSEN <jonas.vermassen.y1164@student.hogent.be>
2019-12-30 10:20:53 +0100

    implemented adapter for monster list

--- app/src/main/java/com/example/initiativetracker/ui/MonsterListAdapter.kt
+++ app/src/main/java/com/example/initiativetracker/ui/MonsterListAdapter.kt
@@ -1,25 +1,43 @@
 package com.example.initiativetracker.ui
 
-import android.view.View
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.example.initiativetracker.databinding.ListItemMonsterBinding
 import com.example.initiativetracker.domain.Monster
+import com.example.initiativetracker.ui.MonsterListAdapter.ViewHolder.Companion.from
+import com.example.initiativetracker.util.OnItemClickListener
 
-class MonsterListAdapter :
-    ListAdapter<Monster, MonsterListAdapter.MonsterViewHolder>(DiffCallback()) {
+class MonsterListAdapter(val listener: OnItemClickListener<Monster>) :
+    ListAdapter<Monster, MonsterListAdapter.ViewHolder>(DiffCallback()) {
 
-    class MonsterViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    class ViewHolder private constructor(val binding: ListItemMonsterBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Monster, listener: OnItemClickListener<Monster>) {
+            binding.monster = item
+            itemView.setOnClickListener {
+                listener.onItemClick(item)
+            }
+        }
 
+        companion object {
+            fun from(parent: ViewGroup): ViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ListItemMonsterBinding.inflate(layoutInflater, parent, false)
+                return ViewHolder(binding)
+            }
+        }
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MonsterViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return from(parent)
     }
 
-    override fun onBindViewHolder(holder: MonsterViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val monster = getItem(position)
+        holder.bind(monster, listener)
     }
 
     class DiffCallback : DiffUtil.ItemCallback<Monster>() {
---
+++ app/src/main/java/com/example/initiativetracker/util/OnItemClickListener.kt
@@ -0,0 +1,5 @@
+package com.example.initiativetracker.util
+
+interface OnItemClickListener<T> {
+    fun onItemClick(item: T)
+}
